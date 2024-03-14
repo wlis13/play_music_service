b74fd7e92dea810ca358c2d6069505d0b76044b1
wlisses <wlissesfernando285@gmail.com>
2024-03-14 11:40:44 -0300

    nothing alse

--- src/services/addMusicDb.service.js
+++ src/services/addMusicDb.service.js
@@ -16,7 +16,7 @@ async function updateMusicService(id, updateValue) {
 }
 
 async function addMusicService(newMusic) {
-  await Music.insertMany(newMusic);
+  await Music.insertOne(newMusic);
   return `Musica ${newMusic.title} foi adicionada com sucesso!`
 }
 
